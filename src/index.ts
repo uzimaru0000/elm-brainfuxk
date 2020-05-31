f6ff7b34232bc09ef8c272f346e6d7090093fb78
uzimaru0000 <shuji365630@gmail.com>
2020-05-31 13:34:32 +0900

    :green_heart: add build command

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 // @ts-ignore
 import { Elm } from './Elm/Main.elm';
-import './style.css';
 
-const app = Elm.Main.init({ node: document.getElementById('main') });
+Elm.Main.init({ node: document.getElementById('main') });
